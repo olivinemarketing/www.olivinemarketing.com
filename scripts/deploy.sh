d7989c738651e3a93e7727b2475162070c6698ae
Santiago Suarez Ordoñez <santiycr@gmail.com>
2017-11-08 13:00:07 -0800

    improved travis setup and adding cloudfront cache invalidation

---
+++ scripts/deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+: "${DISTRIBUTION_ID:?Need to set DISTRIBUTION_ID non-empty}"
+: "${AWS_ACCESS_KEY_ID:?Need to set AWS_ACCESS_KEY_ID non-empty}"
+: "${AWS_SECRET_ACCESS_KEY:?Need to set AWS_SECRET_ACCESS_KEY non-empty}"
+if [[ $1 == "production" ]]
+then
+  cactus deploy
+  aws cloudfront create-invalidation --distribution-id $DISTRIBUTION_ID --paths '/*'
+else
+  cactus build
+fi
